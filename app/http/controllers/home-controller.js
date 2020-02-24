a28b9aa64a622df08fd5ac66068f519415e9ac5f
thanhan7914 <thanhan7914@gmail.com>
2020-02-24 20:58:26 +0700

    category

--- app/http/controllers/home-controller.js
+++ app/http/controllers/home-controller.js
@@ -51,6 +51,38 @@ class HomeController extends Controller {
         if (article == null) return res.redirect("/");
         res.render("single", { article });
     }
+
+    async category(req ,res) {
+        let page = req.get("page") ? req.get("page") : 1;
+        if (page < 1) page = 1;
+        let skip = (page - 1) * 10;
+        let result = await Post
+            .join('post_category', 'posts.id', '=', 'post_category.post_id')
+            .join('categories', 'post_category.cat_id', '=', 'categories.id')
+            .where("posts.publication", true)
+            .where("categories.slug", req.params.slug)
+            .offset(skip)
+            .limit(10)
+            .orderBy("posts.created_at", "desc")
+            .getData(Post);
+
+        let count = (await Post.count("* as count").first()).count;
+        let pages = Math.ceil(count / 10);
+        pages = pages > MAX_PAGES ? MAX_PAGES : pages;
+        let numbers = [];
+        for (let i = 1; i <= pages; i++) {
+            if (!(i > 1 && i < pages && Math.abs(i - page) > 2))
+                numbers.push(i);
+        }
+
+        res.render("index", {
+            posts: result,
+            count: count,
+            pages: pages,
+            page: page,
+            numbers: numbers
+        });
+    }
 }
 
 module.exports = HomeController;
